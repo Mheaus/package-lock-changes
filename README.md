319541798995d438cdcff1457f5b6208b8369e34
Bartosz Kaszubowski <gosimek@gmail.com>
2021-04-26 15:48:32 +0200

    history from previous version

--- README.md
+++ README.md
@@ -1,22 +1,33 @@
 # Yarn Lock Changes
 
-Creates a comment inside Pull Request with the human-readable summary of changes to the `yarn.lock`.
+Creates a comment inside Pull Request with the human-readable summary of the changes to the `yarn.lock` file.
 
-<img width="506" alt="Screenshot 2021-04-26 154252" src="https://user-images.githubusercontent.com/719641/116092424-17dbe100-a6a6-11eb-9725-78bf42f836b4.png">
+## Preview
+
+### Short list of changes
+
+<img alt="Screenshot 2021-04-28 120655" src="https://user-images.githubusercontent.com/719641/116386618-3f53ba80-a81a-11eb-9438-6601d401f880.png">
+
+### Long list of changes
+
+<img alt="Screenshot 2021-04-28 115019" src="https://user-images.githubusercontent.com/719641/116385385-09620680-a819-11eb-857c-113f9048d856.png">
 
 ## Usage
 
 ```yml
-name: Yarn Lock Changes
-uses: ./
-with:
-  path: 'yarn.lock'
-  token: ${{ secrets.GITHUB_TOKEN }}
+- name: Yarn Lock Changes
+  # for now, use `main` before the stable release will be published as `v1`
+  uses: Simek/yarn-lock-changes@main 
+  with:
+    path: 'yarn.lock'
+    token: ${{ secrets.GITHUB_TOKEN }}
 ```
 
 ## Inputs
 
-| Input | Required | Description |
-| --- | --- | --- |
-| `path` | No | Path in repository to the `yarn.lock` file. Default `yarn.lock` (project root). |
-| `token` | **Yes** | GitHub token for the bot, so it can publish a comment in the pull request. |
+| Input | Required | Default | Description |
+| --- | :---: | :---: | --- |
+| `collapsibleThreshold` | No | `'25'` | Number of lock changes, which will result in collapsed comment content an addition of summary table. |
+| `path` | No | `'yarn.lock'` | Path to the `yarn.lock` file in the repository. Default value points to the file at project root. |
+| `token` | **Yes** | - | GitHub token for the bot, so it can publish a comment in the pull request. |
+| `updateComment` | No | `'true'` | Should the bot update the summary comment. If value is `'false'`, bot will post a new comment on each new commit. |
--- action.js
+++ action.js
@@ -1,105 +1,217 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const lockfile = require("@yarnpkg/lockfile");
+const lockfile = require('@yarnpkg/lockfile');
+const compareVersions = require('compare-versions');
 const fs = require('fs');
-const path = require('path');
+const { Base64 } = require('js-base64');
 const { markdownTable } = require('markdown-table');
-const fetch = require('node-fetch');
+const path = require('path');
 
-const diff = (previous, current) => {
+const GH_RAW_URL = 'https://raw.githubusercontent.com';
+const ASSETS_URL = `${GH_RAW_URL}/Simek/yarn-lock-changes/main/assets`;
+const COMMENT_HEADER = '## `yarn.lock` changes';
+
+const getStatusLabel = (status) =>
+  `[<sub><img alt="${status.toUpperCase()}" src="${ASSETS_URL}/${status}.svg" height="16" /></sub>](#)`;
+
+const formatEntry = (obj) =>
+  Object.fromEntries(
+    Object.keys(obj.object).map((key) => {
+      const nameParts = key.split('@');
+      const name = nameParts[0] === '' ? '@' + nameParts[1] : nameParts[0];
+      return [name, { name, version: obj.object[key].version }];
+    })
+  );
+
+const diffLocks = (previous, current) => {
   const changes = {};
-  const previousPackages = formatNameAndVersion(previous);
-  const currentPackages = formatNameAndVersion(current);
+  const previousPackages = formatEntry(previous);
+  const currentPackages = formatEntry(current);
 
   Object.keys(previousPackages).forEach((key) => {
     changes[key] = {
       previous: previousPackages[key].version,
-      current: "-",
-      status: "🗑️ **REMOVED**"
+      current: '-',
+      status: 'removed'
     };
   });
 
   Object.keys(currentPackages).forEach((key) => {
     if (!changes[key]) {
       changes[key] = {
-        previous: "-",
+        previous: '-',
         current: currentPackages[key].version,
-        status: "✨ **NEW**"
+        status: 'added'
       };
     } else {
       if (changes[key].previous === currentPackages[key].version) {
         delete changes[key];
       } else {
         changes[key].current = currentPackages[key].version;
-        changes[key].status = "⬆️ **UPDATED**";
+        if (compareVersions(changes[key].previous, changes[key].current) === 1) {
+          changes[key].status = 'downgraded';
+        } else {
+          changes[key].status = 'updated';
+        }
       }
     }
   });
 
   return changes;
-}
+};
 
-const formatNameAndVersion = (obj) => {
-  const packages = {};
+const createTable = (lockChanges) =>
+  markdownTable(
+    [
+      ['Name', 'Status', 'Previous', 'Current'],
+      ...Object.entries(lockChanges)
+        .map(([key, { status, previous, current }]) => [
+          '`' + key + '`',
+          getStatusLabel(status),
+          previous,
+          current
+        ])
+        .sort((a, b) => a[0].localeCompare(b[0]))
+    ],
+    { align: ['l', 'c', 'c', 'c'], alignDelimiters: false }
+  );
 
-  Object.keys(obj.object).forEach((key) => {
-    const names = key.split("@");
-    const name = names[0] === "" ? "@" + names[1] : names[0];
-    packages[name] = { name, version: obj.object[key].version };
-  });
+const countStatuses = (lockChanges, statusToCount) =>
+  Object.values(lockChanges).filter(({ status }) => status === statusToCount).length;
 
-  return packages;
-}
+const createSummaryRow = (lockChanges, status) => {
+  const statusCount = countStatuses(lockChanges, status);
+  return statusCount ? [getStatusLabel(status), statusCount] : undefined;
+};
 
-async function run() {
+const createSummary = (lockChanges) =>
+  markdownTable(
+    [
+      ['Status', 'Count'],
+      createSummaryRow(lockChanges, 'added'),
+      createSummaryRow(lockChanges, 'updated'),
+      createSummaryRow(lockChanges, 'downgraded'),
+      createSummaryRow(lockChanges, 'removed')
+    ].filter(Boolean),
+    { align: ['l', 'c'], alignDelimiters: false }
+  );
+
+const getBooleanInput = (input) => {
+  const trueValues = ['true', 'yes', 'y', 'on'];
+  const falseValues = ['false', 'no', 'n', 'off'];
+  const stringInput = core.getInput(input).toLowerCase();
+
+  if (trueValues.includes(stringInput)) {
+    return true;
+  } else if (falseValues.includes(stringInput)) {
+    return false;
+  }
+
+  throw TypeError(`💥 Wrong boolean value of the input '${input}', aborting!`);
+};
+
+const run = async () => {
   try {
-    const octokit = github.getOctokit(core.getInput('token'));
+    const octokit = github.getOctokit(core.getInput('token', { required: true }));
+    const inputPath = core.getInput('path');
+    const updateComment = getBooleanInput('updateComment');
+    const collapsibleThreshold = Math.max(parseInt(core.getInput('collapsibleThreshold'), 10), 0);
+
     const { owner, repo, number } = github.context.issue;
-    const { default_branch } = github.context.payload.repository;
 
     if (!number) {
-      throw new Error('Cannot find the PR!');
+      throw new Error('💥 Cannot find the PR, aborting!');
     }
 
-    const lockPath = path.resolve(process.cwd(), core.getInput('path'));
+    const lockPath = path.resolve(process.cwd(), inputPath);
 
     if (!fs.existsSync(lockPath)) {
-      throw new Error(`${lockPath} does not exist!`)
+      throw new Error('💥 It looks like lock does not exist in this PR, aborting!');
     }
 
     const content = await fs.readFileSync(lockPath, { encoding: 'utf8' });
     const updatedLock = lockfile.parse(content);
 
-    console.log(`https://raw.githubusercontent.com/${owner}/${repo}/${default_branch}/${core.getInput('path')}`)
-
-    const response = await fetch(`https://raw.githubusercontent.com/Simek/wikitaxa/master/${core.getInput('path')}`);
-
-    if (!response) {
-      throw new Error('Cannot fetch current lock file!');
-    }
-
-    const masterLock = lockfile.parse(await response.text());
-
-    const lockChanges = diff(masterLock, updatedLock);
-    console.warn(lockChanges)
-
-    const diffsTable = markdownTable([
-      ['Name', 'Status', 'Previous', 'Current'],
-      ...Object.entries(lockChanges).map(([key, value]) => (
-        ['`' + key + '`', value.status, value.previous, value.current]
-      )).sort((a, b) => a[0].localeCompare(b[0]))
-    ])
-
-    await octokit.issues.createComment({
+    const masterLockResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
       owner,
       repo,
-      issue_number: number,
-      body: '## `yarn.lock` changes' + '\n' + diffsTable
+      path: inputPath
     });
 
+    if (!masterLockResponse || !masterLockResponse.data || !masterLockResponse.data.content) {
+      throw new Error('💥 Cannot fetch base lock, aborting!');
+    }
+
+    const masterLock = lockfile.parse(Base64.decode(masterLockResponse.data.content));
+    const lockChanges = diffLocks(masterLock, updatedLock);
+    const lockChangesCount = Object.keys(lockChanges).length;
+
+    if (lockChangesCount) {
+      const diffsTable = createTable(lockChanges);
+      const collapsed = lockChangesCount >= collapsibleThreshold;
+
+      const changesSummary = collapsed ? '### Summary\n' + createSummary(lockChanges) : '';
+
+      const commentBody =
+        COMMENT_HEADER +
+        '\n' +
+        changesSummary +
+        '\n' +
+        '<details' +
+        (collapsed ? '' : ' open') +
+        '>\n' +
+        '<summary>Click to toggle table visibility</summary>\n<br/>\n\n' +
+        diffsTable +
+        '\n\n' +
+        '</details>';
+
+      if (updateComment) {
+        const currentComments = await octokit.issues.listComments({
+          owner,
+          repo,
+          issue_number: number,
+          per_page: 100
+        });
+
+        if (!currentComments || !currentComments.data) {
+          throw new Error('💥 Cannot fetch PR comments, aborting!');
+        }
+
+        const commentId = currentComments.data
+          .filter(
+            (comment) =>
+              comment.user.login === 'github-actions[bot]' &&
+              comment.body.startsWith(COMMENT_HEADER)
+          )
+          .map((comment) => comment.id)[0];
+
+        if (commentId) {
+          await octokit.issues.updateComment({
+            owner,
+            repo,
+            comment_id: commentId,
+            body: commentBody
+          });
+        } else {
+          await octokit.issues.createComment({
+            owner,
+            repo,
+            issue_number: number,
+            body: commentBody
+          });
+        }
+      } else {
+        await octokit.issues.createComment({
+          owner,
+          repo,
+          issue_number: number,
+          body: commentBody
+        });
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
 run();
